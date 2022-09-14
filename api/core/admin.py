4619183f85f76219721a58ef50fdd52630fdb57c
Cthupby <Cthupby@gmail.com>
2022-09-14 22:59:05 +0300

    add post admin

--- api/core/admin.py
+++ api/core/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Post
 
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('date', 'title', 'amount', 'distance')
+    search_fields = ['title', 'distance']
+
+admin.site.register(PostAdmin)
