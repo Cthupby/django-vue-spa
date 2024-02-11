039f8736e756733d927292cc591343f0cffadc36
Cthupby <Cthupby@gmail.com>
2024-02-11 14:00:35 +0300

    Update api settings

--- api/api/settings.py
+++ api/api/settings.py
@@ -21,14 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY", "SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
 # 'DJANGO_ALLOWED_HOSTS' should be a single string of hosts with a space between each.
 # For example: 'DJANGO_ALLOWED_HOSTS=localhost 127.0.0.1 [::1]'
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", ["*"])
 
 
 # Application definition
@@ -85,8 +85,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'django_vue_spa',
-        'USER': 'cthupby',
+        'USER': 'admin',
         'PASSWORD': 'M4Bn5G9n',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
